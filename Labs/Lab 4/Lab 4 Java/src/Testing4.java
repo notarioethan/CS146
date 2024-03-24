e5d09a168a515a148aa1008e3ff298d6270cdec6
notarioethan <133898526+notarioethan@users.noreply.github.com>
2024-03-24 15:57:17 -0700

    lab 4 java updates

--- Labs/Lab 4/Lab 4 Java/src/Testing4.java
+++ Labs/Lab 4/Lab 4 Java/src/Testing4.java
@@ -1,17 +1,39 @@
 public class Testing4 {
-    public static void main(String[] args) {
-		TreeNode4 six = new TreeNode4(6);
-		TreeNode4 five = new TreeNode4(5);
-		TreeNode4 three = new TreeNode4(3);
-		TreeNode4 four = new TreeNode4(4);
-		TreeNode4 eight = new TreeNode4(8, five, six);
-		TreeNode4 two = new TreeNode4(2, three, four);
-		TreeNode4 one = new TreeNode4(1, two, eight);
-		TreeNode4.printer(one);
-		one = TreeNode4.invertTree(one);
+    public static TreeNode invertTree(TreeNode root) {
+    	inversion(root);
+    	return root;
+    }
+    private static void inversion(TreeNode node) {
+    	if (node == null) return;
+    	TreeNode temp = node.right;
+    	node.right = node.left;
+    	node.left = temp;
+        inversion(node.right);
+    	inversion(node.left);
+    }
+    public static void printer(TreeNode node) {//in order traversal
+    	printrecursive(node);
+    	System.out.println();
+    }
+    private static void printrecursive(TreeNode node) {
+    	if (node == null) return;
+    	printrecursive(node.left);
+    	System.out.print(node.val + " ");
+    	printrecursive(node.right);
+    }
+	public static void main(String[] args) {
+		TreeNode six = new TreeNode(6);
+		TreeNode five = new TreeNode(5);
+		TreeNode three = new TreeNode(3);
+		TreeNode four = new TreeNode(4);
+		TreeNode eight = new TreeNode(8, five, six);
+		TreeNode two = new TreeNode(2, three, four);
+		TreeNode one = new TreeNode(1, two, eight);
+		printer(one);
+		one = invertTree(one);
 		
-		TreeNode4.printer(one);
-		one = TreeNode4.invertTree(one);
-		TreeNode4.printer(one);
+		printer(one);
+		one = invertTree(one);
+		printer(one);
 	}
 }
