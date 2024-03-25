ccb28faa8f4cccc1e85547b28196439c9bc0019f
notarioethan <133898526+notarioethan@users.noreply.github.com>
2024-03-24 21:07:17 -0700

    lol

--- Labs/Lab 5/Lab 5 Java/src/Testing5.java
+++ Labs/Lab 5/Lab 5 Java/src/Testing5.java
@@ -1,21 +1,31 @@
 public class Testing5 {
+    public static boolean isValidBST(TreeNode root) {
+        return validator(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+    private static boolean validator(TreeNode node, int min, int max){
+        if (node == null) return true;
+        boolean a = validator(node.left, min, node.val);
+        boolean b = validator(node.right, node.val, max);
+        return a && b && min <= node.val && node.val <= max;
+    }
+    
     public static void main(String[] args) throws Exception {
         //test case 1
-        TreeNode5 oneA = new TreeNode5(1);
-        TreeNode5 fiveA = new TreeNode5(5);
-        TreeNode5 nineA = new TreeNode5(9);
-        TreeNode5 threeA = new TreeNode5(3, oneA, null);
-        TreeNode5 eightA = new TreeNode5(8, fiveA, nineA);
-        TreeNode5 fourA = new TreeNode5(4, threeA, eightA);
-        System.out.println(TreeNode5.isValidBST(fourA));
+        TreeNode oneA = new TreeNode(1);
+        TreeNode fiveA = new TreeNode(5);
+        TreeNode nineA = new TreeNode(9);
+        TreeNode threeA = new TreeNode(3, oneA, null);
+        TreeNode eightA = new TreeNode(8, fiveA, nineA);
+        TreeNode fourA = new TreeNode(4, threeA, eightA);
+        System.out.println(isValidBST(fourA));
         //test case 2
-        TreeNode5 threeB = new TreeNode5(3);
-        TreeNode5 fourB = new TreeNode5(4);
-        TreeNode5 fiveB = new TreeNode5(5);
-        TreeNode5 sixB = new TreeNode5(6);
-        TreeNode5 twoB = new TreeNode5(2, threeB, fourB);
-        TreeNode5 eightB = new TreeNode5(8, fiveB, sixB);
-        TreeNode5 oneB = new TreeNode5(1, twoB, eightB);
-        System.out.println(TreeNode5.isValidBST(oneB));
+        TreeNode threeB = new TreeNode(3);
+        TreeNode fourB = new TreeNode(4);
+        TreeNode fiveB = new TreeNode(5);
+        TreeNode sixB = new TreeNode(6);
+        TreeNode twoB = new TreeNode(2, threeB, fourB);
+        TreeNode eightB = new TreeNode(8, fiveB, sixB);
+        TreeNode oneB = new TreeNode(1, twoB, eightB);
+        System.out.println(isValidBST(oneB));
     }
 }
