daedfbadfdf94b079e3b3d04bd6659af541ce64e
notarioethan <133898526+notarioethan@users.noreply.github.com>
2024-03-24 20:41:57 -0700

    lab 4 python

--- Labs/Lab 4/Lab 4 explanation.md
+++ Labs/Lab 4/Lab 4 explanation.md
@@ -1,8 +1,15 @@
-Lab 4
+#Lab 4
 
-Problem Statement: Given the root of a binary tree, invert the tree, and return its root.
+##Problem Statement: Given the root of a binary tree, invert the tree, and return its root.
 
-Approach:
-- use recursive helper function to traverse through binary tree
+###Approach:
+- use recursive helper function to traverse through binary tree (no helper method for Python)
 - for every node, swap left and right pointers
 - return root
+
+##Follow-up exercise: Can you think of a way to do this iteratively?
+###Approach(not implemented):
+- create a stack containing the root
+- use while loop (while stack not empty)
+- pop a node off the stack, swap left and right pointers
+- push left and right children onto the stack if they exist
---
+++ Labs/Lab 4/lab4.py
@@ -0,0 +1,48 @@
+class TreeNode(object):
+
+     def __init__(self, val=0, left=None, right=None):
+
+         self.val = val
+
+         self.left = left
+
+         self.right = right
+
+class Solution:
+    
+    
+    def invertTree(self, root):
+        #Your code here
+        if root == None:
+            return
+        temp = root.left
+        root.left = root.right
+        root.right = temp
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
+    
+    
+
+    def printTree(self, node):
+        if node == None:
+            return
+        self.printTree(node.left)
+        print(node.val, end = ' ')
+        self.printTree(node.right)
+        
+
+if __name__ == '__main__':
+    #test case 1
+    
+    three = TreeNode(3)
+    four = TreeNode(4)
+    five = TreeNode(5)
+    six = TreeNode(6)
+    two = TreeNode(2, three, four)
+    eight = TreeNode(8, five, six)
+    one = TreeNode(1, two, eight)
+    Solution().printTree(one)
+    one = Solution().invertTree(one)
+    print("\n")
+    Solution().printTree(one)
