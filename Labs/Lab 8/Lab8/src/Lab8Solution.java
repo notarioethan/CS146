e22821cf6d1393c7e26af4355bd33bd1719be8a0
notarioethan <133898526+notarioethan@users.noreply.github.com>
2024-05-06 23:01:12 -0700

    Update Lab8Solution.java

--- Labs/Lab 8/Lab8/src/Lab8Solution.java
+++ Labs/Lab 8/Lab8/src/Lab8Solution.java
@@ -1,9 +1,35 @@
+import java.util.*;
+
 public class Lab8Solution {
     public int coinChange(int[] coins, int amount) {
+        int[] mins = new int[amount + 1];
+        Arrays.fill(mins, amount + 1);
+        mins[0] = 0;
+
+        for (int i = 0; i < coins.length; i++){
+            for (int j = 0; j <= amount; j++){
+                find(coins[i], i, j, mins);
+            }
+        }
+
+        if (mins[amount] > amount) return -1;
+        return mins[amount];
+    }
+    private void find(int cents, int lvl, int amt, int[] mins){
+        int quo = amt/cents;
         
-        return 0;//placeholder
+        for (int i = quo; i >= 0; i--){
+            mins[amt] = Math.min(mins[amt], i + mins[amt - cents * i]);
+        }
     }
     public static void main(String[] args){
-        
+        Lab8Solution s = new Lab8Solution();
+        //test case 1
+        System.out.println(s.coinChange(new int[] {1, 2, 5}, 11));
+        //test case 2
+        System.out.println(s.coinChange(new int[] {2}, 3));
+        //bonus test case
+        System.out.println(s.coinChange(new int[] {186, 419, 83, 408}, 6249));
+        //System.out.println(s.coinChange(new int[] {5, 1, 2}, 11));
     }
 }
